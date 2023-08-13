508b0fc260c79cf1590d9cd8248345aa264d782d
Iris Walter <walteriris@gmx.de>
2023-08-13 12:44:33 +0200

    Add language support

---
+++ src/languages/english.ts
@@ -0,0 +1,25 @@
+export class English {
+    protected constructor() {}
+
+    public static APPLY_COUNTED(num: number, format: string): string {
+        return format.replace("{count}", num.toString())
+    }
+
+    public static TIME_LOCALE = "en"
+    public static TIME_YESTERDAY = "yesterday"
+    public static TIME_HOURS_AGO = "{count} hour(s) ago"
+    public static TIME_MINUTES_AGO = "{count} minute(s) ago"
+    public static TIME_SECONDS_AGO = "{count} second(s) ago"
+    public static TIME_TODAY_AT = "today at"
+    public static TIME_JUST_NOW = "just now"
+
+    public static APPNAME = "Cashbalance"
+
+    public static LOGIN_KNOWN_CONNECTIONS = "Reuse Connection"
+    public static LOGIN_SESSION_NAME = "Session Name"
+    public static LOGIN_API_ENDPOINT_LABEL = "API Endpoint URL"
+    public static LOGIN_API_TOKEN_LABEL = "API Token"
+    public static LOGIN_SUBMIT = "Add Connection"
+    public static LOGIN_ERROR_MISSING_INPUTS = "You must provide all fields (session name, api endpoint and api token)."
+    public static LOGIN_ERROR_LOGIN_FAILED = "The provided login information is wrong. Is the URL and API token correct?"
+}
