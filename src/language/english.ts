dcd5e028d1d1b42290db73fbc9175fa5c67c0cd7
Iris Walter <walteriris@gmx.de>
2023-08-20 20:09:32 +0200

    Add ui strings for entry view

--- src/language/english.ts
+++ src/language/english.ts
@@ -22,4 +22,23 @@ export class English {
     public static LOGIN_SUBMIT = "Add Connection"
     public static LOGIN_ERROR_MISSING_INPUTS = "You must provide all fields (session name, api endpoint and api token)."
     public static LOGIN_ERROR_LOGIN_FAILED = "The provided login information is wrong. Is the URL and API token correct?"
+
+    public static ENTRY_CAPTION_CATEGORY = "Category"
+    public static ENTRY_CAPTION_SHOP = "Shop"
+    public static ENTRY_CAPTION_AMOUNT = "Amount"
+    public static ENTRY_CAPTION_RECEIPT = "Receipt"
+    public static ENTRY_CAPTION_COST_CENTER = "Cost center"
+    public static ENTRY_CAPTION_NOTE = "Note"
+    public static ENTRY_RADIO_IN = "In"
+    public static ENTRY_RADIO_OUT = "Out"
+    public static ENTRY_CHECKBOX_CASH = "Cash"
+    public static ENTRY_CHECKBOX_TAX = "Tax"
+    public static ENTRY_CHECKBOX_DRAFT = "Draft"
+    public static ENTRY_BUTTON_SAVE = "Save"
+    public static ENTRY_PLACEHOLDER_CATEGORY = "Alimentary"
+    public static ENTRY_PLACEHOLDER_SHOP = "Rewe"
+    public static ENTRY_PLACEHOLDER_AMOUNT = "0.00"
+    public static ENTRY_PLACEHOLDER_RECEIPT = "/path/to/receipt.png"
+    public static ENTRY_PLACEHOLDER_NOTE = ""
+    public static ENTRY_LIST_COST_CENTER = ["XF", "MF", "IF", "Other"]
 }
--- src/language/german.ts
+++ src/language/german.ts
@@ -4,4 +4,22 @@ export class German extends English {
     public static TIME_LOCALE = "de"
 
     //public static APPNAME = "Kassenbuch"
+
+    public static ENTRY_CAPTION_CATEGORY = "Kategorie"
+    public static ENTRY_CAPTION_SHOP = "Handelspartner"
+    public static ENTRY_CAPTION_AMOUNT = "Betrag"
+    public static ENTRY_CAPTION_RECEIPT = "Beleg"
+    public static ENTRY_CAPTION_COST_CENTER = "Kostenstelle"
+    public static ENTRY_CAPTION_NOTE = "Notiz"
+    public static ENTRY_RADIO_IN = "Ein"
+    public static ENTRY_RADIO_OUT = "Aus"
+    public static ENTRY_CHECKBOX_CASH = "Bar"
+    public static ENTRY_CHECKBOX_TAX = "Steuerrelevant"
+    public static ENTRY_CHECKBOX_DRAFT = "Entwurf"
+    public static ENTRY_BUTTON_SAVE = "Speichern"
+    public static ENTRY_PLACEHOLDER_CATEGORY = "Lebensmittel & Haushalt"
+    public static ENTRY_PLACEHOLDER_SHOP = "Rewe"
+    public static ENTRY_PLACEHOLDER_AMOUNT = "0.00"
+    public static ENTRY_PLACEHOLDER_RECEIPT = "/path/to/receipt.png"
+    public static ENTRY_LIST_COST_CENTER = ["XF", "MF", "IF", "Andere"]
 }
