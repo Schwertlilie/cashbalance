bfa6edd506deecaa6fd0904c80e8c990b65fdb56
Iris Walter <walteriris@gmx.de>
2023-08-21 08:30:07 +0200

    Generate csv string from entry, missing uuid

--- src/views/entry.ts
+++ src/views/entry.ts
@@ -6,33 +6,42 @@ import { PageManager } from '../webui/pagemanager';
 import './entry.css'
 
 export class Entry extends Module<HTMLDivElement> {
+    private categoryInput: FormInput
+    private shopInput: FormInput
+    private amountInput: FormInput
+    private cashCheckbox: FormCheckbox
+    private taxCheckbox: FormCheckbox
+    private draftCheckbox: FormCheckbox
+    private costCenterRadioButtonGroup: FormRadioButtonGroup
+    private costCenterOtherInput: FormInput
+    private noteInput: FormInput
     public constructor() {
         super("div", "", "entry")
         
         // Category
         let categoryCaption = new FormLabel(STRINGS.ENTRY_CAPTION_CATEGORY, "entryCaption")
-        let categoryInput = new FormInput("inputCategory", STRINGS.ENTRY_PLACEHOLDER_CATEGORY, "text")
-        categoryInput.onChange = (value: string) => {
-            categoryInput.value(value.replaceAll(";", ","))
+        this.categoryInput = new FormInput("inputCategory", STRINGS.ENTRY_PLACEHOLDER_CATEGORY, "text")
+        this.categoryInput.onChange = (value: string) => {
+            this.categoryInput.value(value.replaceAll(";", ","))
         }
         this.add(categoryCaption)
-        this.add(categoryInput)
+        this.add(this.categoryInput)
 
         // Shop
         let shopCaption = new FormLabel(STRINGS.ENTRY_CAPTION_SHOP, "entryCaption")
-        let shopInput = new FormInput("inputShop", STRINGS.ENTRY_PLACEHOLDER_SHOP, "text")
-        shopInput.onChange = (value: string) => {
-            shopInput.value(value.replaceAll(";", ","))
+        this.shopInput = new FormInput("inputShop", STRINGS.ENTRY_PLACEHOLDER_SHOP, "text")
+        this.shopInput.onChange = (value: string) => {
+            this.shopInput.value(value.replaceAll(";", ","))
         }
         this.add(shopCaption)
-        this.add(shopInput)
+        this.add(this.shopInput)
 
         // Amount
         let amountCaption = new FormLabel(STRINGS.ENTRY_CAPTION_AMOUNT, "entryCaption")
         let radioButtonGroupAmount = new FormRadioButtonGroup("radioAmount", [STRINGS.ENTRY_RADIO_IN, STRINGS.ENTRY_RADIO_OUT], "entryRadioGroup")
-        let amountInput = new FormInput("inputAmount", STRINGS.ENTRY_PLACEHOLDER_AMOUNT, "number")
+        this.amountInput = new FormInput("inputAmount", STRINGS.ENTRY_PLACEHOLDER_AMOUNT, "number")
         let amountCurrencyLabel = new FormLabel(" €")
-        amountInput.onChange = (value: string) => {
+        this.amountInput.onChange = (value: string) => {
             let amount = Number(value)
             if (amount < 0) {
                 radioButtonGroupAmount.value(1)
@@ -40,30 +49,31 @@ export class Entry extends Module<HTMLDivElement> {
                 radioButtonGroupAmount.value(0)
             }
         }
-        amountInput.onChangeDone = (value: string) => {
+        this.amountInput.onChangeDone = (value: string) => {
             let reducedValue = Number(value).toFixed(2)
             if (value != reducedValue) {
-                amountInput.value(reducedValue)
+                this.amountInput.value(reducedValue)
             }
+            console.log("Wrote amount: " + this.amountInput.value())
         }
         radioButtonGroupAmount.onChange = (_selectedIndex: number) => {
-            let amount = Number(amountInput.htmlElement.value)
-            amountInput.value((amount * -1).toFixed(2))
+            let amount = Number(this.amountInput.htmlElement.value)
+            this.amountInput.value((amount * -1).toFixed(2))
         }
         this.add(amountCaption)
         this.add(radioButtonGroupAmount)
-        this.add(amountInput)
+        this.add(this.amountInput)
         this.add(amountCurrencyLabel)
         
         // Tags
         let tagDiv = new Module<HTMLDivElement>("div", "", "entryTag")
-        let cashCheckbox = new FormCheckbox("checkboxCash", iconCash + " " + STRINGS.ENTRY_CHECKBOX_CASH, "entryIcon", false)
-        let taxCheckbox = new FormCheckbox("checkboxTax", iconTax + " " + STRINGS.ENTRY_CHECKBOX_TAX, "entryIcon", false)
-        let draftCheckbox = new FormCheckbox("checkboxDraft", STRINGS.ENTRY_CHECKBOX_DRAFT, "", true)
+        this.cashCheckbox = new FormCheckbox("checkboxCash", iconCash + " " + STRINGS.ENTRY_CHECKBOX_CASH, "entryIcon", false)
+        this.taxCheckbox = new FormCheckbox("checkboxTax", iconTax + " " + STRINGS.ENTRY_CHECKBOX_TAX, "entryIcon", false)
+        this.draftCheckbox = new FormCheckbox("checkboxDraft", STRINGS.ENTRY_CHECKBOX_DRAFT, "", true)
         this.add(tagDiv)
-        tagDiv.add(cashCheckbox)
-        tagDiv.add(taxCheckbox)
-        tagDiv.add(draftCheckbox)
+        tagDiv.add(this.cashCheckbox)
+        tagDiv.add(this.taxCheckbox)
+        tagDiv.add(this.draftCheckbox)
         
         // Receipt
         let receiptCaption = new FormLabel(STRINGS.ENTRY_CAPTION_RECEIPT, "entryCaption")
@@ -76,42 +86,67 @@ export class Entry extends Module<HTMLDivElement> {
 
         // Cost center
         let costCenterCaption = new FormLabel(STRINGS.ENTRY_CAPTION_COST_CENTER, "entryCaption")
-        let radioButtonGroupCostCenter = new FormRadioButtonGroup("radioCostCenter", STRINGS.ENTRY_LIST_COST_CENTER, "entryRadioGroup")
+        this.costCenterRadioButtonGroup = new FormRadioButtonGroup("radioCostCenter", STRINGS.ENTRY_LIST_COST_CENTER, "entryRadioGroup")
         let costCenterOtherLabel = new FormLabel(STRINGS.ENTRY_LIST_COST_CENTER[3] + ": ")
-        let costCenterOtherInput = new FormInput("inputCostCenterOther", STRINGS.ENTRY_LIST_COST_CENTER[3], "text")
+        this.costCenterOtherInput = new FormInput("inputCostCenterOther", STRINGS.ENTRY_LIST_COST_CENTER[3], "text")
         costCenterOtherLabel.hide()
-        costCenterOtherInput.hide()
-        costCenterOtherInput.onChange = (value: string) => {
-            costCenterOtherInput.value(value.replaceAll(";", ","))
+        this.costCenterOtherInput.hide()
+        this.costCenterOtherInput.onChange = (value: string) => {
+            this.costCenterOtherInput.value(value.replaceAll(";", ","))
         }
-        radioButtonGroupCostCenter.onChange = (selectedIndex: number) => {
-            if (selectedIndex == 3) {
+        this.costCenterRadioButtonGroup.onChange = (selectedIndex: number) => {
+            if (selectedIndex == STRINGS.ENTRY_LIST_COST_CENTER.length - 1) {
                 costCenterOtherLabel.show()
-                costCenterOtherInput.show()
+                this.costCenterOtherInput.show()
             } else {
                 costCenterOtherLabel.hide()
-                costCenterOtherInput.hide()
+                this.costCenterOtherInput.hide()
             }
         }
         this.add(costCenterCaption)
-        this.add(radioButtonGroupCostCenter)
+        this.add(this.costCenterRadioButtonGroup)
         this.add(costCenterOtherLabel)
-        this.add(costCenterOtherInput)
+        this.add(this.costCenterOtherInput)
         
         // Notes
         let noteCaption = new FormLabel(STRINGS.ENTRY_CAPTION_NOTE, "entryCaption")
-        let noteInput = new FormInput("inputNote", STRINGS.ENTRY_PLACEHOLDER_NOTE, "text")
-        noteInput.onChange = (value: string) => {
-            noteInput.value(value.replaceAll(";", ","))
+        this.noteInput = new FormInput("inputNote", STRINGS.ENTRY_PLACEHOLDER_NOTE, "text")
+        this.noteInput.onChange = (value: string) => {
+            this.noteInput.value(value.replaceAll(";", ","))
         }
         this.add(noteCaption)
-        this.add(noteInput)
+        this.add(this.noteInput)
 
         // Save button
         let saveButton = new Button(STRINGS.ENTRY_BUTTON_SAVE, "buttonWide")
         saveButton.onClick = () => {
+            this.saveEntry()
             PageManager.open("transactionList", {})
         }
         this.add(saveButton)
+    }
+
+    private saveEntry(): string {
+        let csvString: string
+        let costCenter: string
+
+        if (this.costCenterRadioButtonGroup.value() == STRINGS.ENTRY_LIST_COST_CENTER.length - 1) {
+            costCenter = this.costCenterOtherInput.value()
+        } else {
+            costCenter = STRINGS.ENTRY_LIST_COST_CENTER[this.costCenterRadioButtonGroup.value()]
+        }
+
+        csvString = [
+            this.categoryInput.value(),
+            this.shopInput.value(),
+            (Number(this.amountInput.value()) * 100).toFixed(0),
+            this.cashCheckbox.value()?"1":"0",
+            this.taxCheckbox.value()?"1":"0",
+            this.draftCheckbox.value()?"1":"0",
+            costCenter,
+            this.noteInput.value(),
+        ].join(";")
+        console.log(csvString)
+        return csvString
     }
 }
